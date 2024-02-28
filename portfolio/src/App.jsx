35f5ff1fd051aed4c74865931aa1fc4f8aae49c2
Hassan Hussain <hassanmehmodhussain@gmail.com>
2024-02-28 15:26:32 +0100

    hello

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+   <div>
+    <h1>Welcome to my portfolio page!</h1>
+    <p>My projects and resume can be accessed via the menu, Enjoy!</p>
+    </div>
+  )
+}
+
+export default App
